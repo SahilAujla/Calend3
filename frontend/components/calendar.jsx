99c6f4cf5e465938f33b4ad2b08c6e28e76bb987
Sahil Aujla <sahilaujla15@gmail.com>
2022-03-04 10:17:42 +0530

    Integrated fronted with backend using ethers.js

--- frontend/components/calendar.jsx
+++ frontend/components/calendar.jsx
@@ -4,6 +4,8 @@ import abi from "../abis/Calend3.json";
 
 import { Box, Button, Slider } from "@material-ui/core";
 import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
+import Dialog from "@mui/material/Dialog";
+import CircularProgress from "@mui/material/CircularProgress";
 
 import {
   ViewState,
@@ -30,28 +32,28 @@ const contract = new ethers.Contract(
 
 console.log(contract);
 
-const schedulerData = [
-  {
-    startDate: "2022-03-02T09:45",
-    endDate: "2022-03-02T11:00",
-    title: "Meeting 1",
-  },
-  {
-    startDate: "2022-03-03T12:00",
-    endDate: "2022-03-03T13:30",
-    title: "Meeting 2",
-  },
-];
-
-const saveAppointment = (data) => {
-  alert("Appointment saved!");
-  console.log(data);
-};
-
 const Calendar = (props) => {
   const [isAdmin, setIsAdmin] = useState(false);
-  const [showAdmin, setShowAdmin] = useState(false);
   const [rate, setRate] = useState(false);
+  const [appointments, setAppointments] = useState([]);
+
+  const [showDialog, setShowDialog] = useState(false);
+  const [showSign, setShowSign] = useState(false);
+  const [mined, setMined] = useState(false);
+  const [transactionHash, setTransactionHash] = useState("");
+
+  const transformAppointmentData = (appointmentData) => {
+    let data = [];
+    appointmentData.forEach((appointment) => {
+      data.push({
+        title: appointment.title,
+        startDate: new Date(appointment.startTime * 1000),
+        endDate: new Date(appointment.endTime * 1000),
+      });
+    });
+
+    setAppointments(data);
+  };
 
   const getData = async () => {
     // get contract owner and set admin if connected account is owner
@@ -60,6 +62,11 @@ const Calendar = (props) => {
 
     const rate = await contract.getRate();
     setRate(ethers.utils.formatEther(rate.toString()));
+
+    const appointmentData = await contract.getAppointments();
+    console.log("got appointments");
+    console.log(appointmentData);
+    transformAppointmentData(appointmentData);
   };
 
   useEffect(() => {
@@ -71,25 +78,142 @@ const Calendar = (props) => {
     setRate(newValue);
   };
 
+  const saveAppointment = async (data) => {
+    const appointment = data.added;
+    const title = appointment.title;
+    const startTime = appointment.startDate.getTime() / 1000;
+    const endTime = appointment.endDate.getTime() / 1000;
+
+    setShowSign(true);
+    setShowDialog(true);
+    setMined(false);
+
+    try {
+      const cost = ((endTime - startTime) / 60) * rate;
+      const msg = { value: ethers.utils.parseEther(cost.toString()) };
+      let transaction = await contract.createAppointment(
+        title,
+        startTime,
+        endTime,
+        msg
+      );
+
+      setShowSign(false);
+
+      await transaction.wait();
+
+      setMined(true);
+      setTransactionHash(transaction.hash);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const saveRate = async () => {
     const tx = await contract.setRate(ethers.utils.parseEther(rate.toString()));
   };
 
+  const marks = [
+    {
+      value: 0.0,
+      label: "Free",
+    },
+    {
+      value: 0.02,
+      label: "0.02 ETH/min",
+    },
+    {
+      value: 0.04,
+      label: "0.04 ETH/min",
+    },
+    {
+      value: 0.06,
+      label: "0.06 ETH/min",
+    },
+    {
+      value: 0.08,
+      label: "0.08 ETH/min",
+    },
+    {
+      value: 0.1,
+      label: "Expensive",
+    },
+  ];
+
   const Admin = () => {
     return (
       <div className="admin">
-        <h3>Hello</h3>
-        <Slider
-          defaultValue={parseFloat(rate)}
-          step={0.001}
-          min={0}
-          max={0.1}
-          valueLabelDisplay="auto"
-          onChangeCommitted={handleSliderChange}
-        />
-        <br />
-        <br />
+        <Box>
+          <h3>Set Your Minutely Rate</h3>
+          <Slider
+            defaultValue={parseFloat(rate)}
+            step={0.001}
+            min={0}
+            max={0.1}
+            marks={marks}
+            valueLabelDisplay="auto"
+            onChangeCommitted={handleSliderChange}
+          />
+          <br />
+          <br />
+          <Button className="settings" onClick={saveRate} variant="contained">
+            <SettingsSuggestIcon /> save configuration
+          </Button>
+        </Box>
       </div>
+    );
+  };
+
+  const ConfirmDialog = () => {
+    return (
+      <Dialog open={true}>
+        <h3>
+          {mined && "Appointment Confirmed"}
+          {!mined && !showSign && "Confirming Your Appointment..."}
+          {!mined && showSign && "Please Sign to Confirm"}
+        </h3>
+        <div style={{ textAlign: "left", padding: "0px 20px 20px 20px" }}>
+          {mined && (
+            <div>
+              Your appointment has been confirmed and is on the blockchain.
+              <br />
+              <br />
+              <a
+                target="_blank"
+                href={`https://goerli.etherscan.io/tx/${transactionHash}`}
+              >
+                View on Etherscan
+              </a>
+            </div>
+          )}
+          {!mined && !showSign && (
+            <div>
+              <p>
+                Please wait while we confirm your appoinment on the
+                blockchain....
+              </p>
+            </div>
+          )}
+          {!mined && showSign && (
+            <div>
+              <p>Please sign the transaction to confirm your appointment.</p>
+            </div>
+          )}
+        </div>
+        <div style={{ textAlign: "center", paddingBottom: "30px" }}>
+          {!mined && <CircularProgress />}
+        </div>
+        {mined && (
+          <Button
+            onClick={() => {
+              setShowDialog(false);
+              getData();
+            }}
+          >
+            Close
+          </Button>
+        )}
+      </Dialog>
     );
   };
 
@@ -97,7 +221,7 @@ const Calendar = (props) => {
     <>
       {isAdmin && <Admin />}
       <div className="calendar">
-        <Scheduler data={schedulerData}>
+        <Scheduler data={appointments}>
           <ViewState />
           <EditingState onCommitChanges={saveAppointment} />
           <IntegratedEditing />
@@ -106,6 +230,8 @@ const Calendar = (props) => {
           <AppointmentForm />
         </Scheduler>
       </div>
+
+      {showDialog && <ConfirmDialog />}
     </>
   );
 };
